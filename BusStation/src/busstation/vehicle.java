66a25cb5be9fa74f6143176db1b530b1649c1018
Mohamedkh988 <75550182+Mohamedkh988@users.noreply.github.com>
2020-12-06 02:38:53 +0200

    Add files via upload

---
+++ BusStation/src/busstation/vehicle.java
@@ -0,0 +1,17 @@
+package busstation;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author omar_
+ */
+public abstract class vehicle {
+    protected int seats;
+    
+    
+}
